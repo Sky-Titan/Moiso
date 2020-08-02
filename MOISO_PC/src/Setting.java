dc4cccad2414d5b719873583f38212b4a90c0b5f
Sky-Titan <er65119@naver.com>
2020-08-02 20:19:10 +0900

    pc앱 생성

---
+++ MOISO_PC/src/Setting.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Setting extends JFrame {
+
+    public Setting()
+    {
+        super("MOISO");
+
+        setLocation(550, 230);
+        setSize(1000, 500);
+        setBackground(Color.WHITE);
+
+        
+
+        setVisible(true);
+    }
+}
